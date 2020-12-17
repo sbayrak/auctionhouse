ebb499c424c65f704f3a80df6bc17025aa9c15d5
sbayrak <suat.bayrak@bilgiedu.net>
2020-12-17 18:35:47 +0300

    Displayed SingleAdvert from Redux

--- client/src/components/adverts/SingleAdvert.js
+++ client/src/components/adverts/SingleAdvert.js
@@ -1,28 +1,120 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import '../../css/singleadvert.css';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 import { getAdvertById } from '../../actions/advert';
+import SingleAdvertComment from './SingleAdvertComment';
 
 const SingleAdvert = ({
   match,
   advert: { advert, loading, comments },
+  auth,
   getAdvertById,
 }) => {
   useEffect(() => {
     getAdvertById(match.params.advertId);
   }, [getAdvertById, match.params.advertId]);
 
-  return <h1> hi</h1>;
+  // console.log(advert); response verdi console'a
+  // if (auth.user !== null) {
+  //   console.log(auth.user.avatar);
+  // } else {
+  //   console.log('user null');
+  // }
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner></Spinner>
+      ) : (
+        <Fragment>
+          {auth.user === null ? (
+            <Spinner></Spinner>
+          ) : (
+            <div className='wrapper6'>
+              <div className='single-advert'>
+                <div className='single-advert-title'>
+                  <div className='single-advert-title-left'>
+                    <img src={auth.user.avatar} alt='' />
+                    <span id='single-advert-title-left-company'>
+                      <a href='#!'>{auth.user.company}</a>
+                    </span>
+                  </div>
+                  <div className='single-advert-title-right'>
+                    <textarea
+                      name='text'
+                      id='text'
+                      cols='30'
+                      rows='5'
+                      disabled
+                      value={advert.text}
+                    ></textarea>
+                    <span id='single-advert-title-right-date'>
+                      Posted on{' '}
+                      <Moment format='DD/MM/YYYY'>{advert.date}</Moment>
+                    </span>
+                  </div>
+                </div>
+
+                <div className='single-advert-bid'>
+                  <input
+                    type='text'
+                    name='bid'
+                    id='bid'
+                    placeholder='Place your bid here...'
+                  />
+                  <button type='button' id='bid-button'>
+                    Submit
+                  </button>
+                </div>
+
+                <div className='single-advert-comment'>
+                  <span id='single-advert-comment-bar'>Leave a comment</span>
+                  <form id='myform'>
+                    <textarea
+                      name='text'
+                      id='text'
+                      cols='30'
+                      rows='10'
+                      placeholder='Commen on this advert...'
+                    ></textarea>
+                    <button type='button' id='comment-button'>
+                      Submit
+                    </button>
+                  </form>
+                </div>
+
+                <div className='single-advert-comments'>
+                  <Fragment>
+                    {advert.comments.map((comment, key) => (
+                      <SingleAdvertComment
+                        comment={comment}
+                        key={comment._id}
+                      ></SingleAdvertComment>
+                    ))}
+                  </Fragment>
+                </div>
+              </div>
+            </div>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 SingleAdvert.propTypes = {
   advert: PropTypes.object,
   getAdvertById: PropTypes.func,
+  auth: PropTypes.object,
 };
 
 const mapStateToProps = (state) => ({
-  advert: state.advert.advert,
+  advert: state.advert,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { getAdvertById })(SingleAdvert);
