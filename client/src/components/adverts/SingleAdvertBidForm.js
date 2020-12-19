ddcd8ccd9cc38268520de69bff89999d061efea5
sbayrak <34121748+sbayrak@users.noreply.github.com>
2020-12-19 16:54:01 +0300

    Added modal for place bid and RegEx

--- client/src/components/adverts/SingleAdvertBidForm.js
+++ client/src/components/adverts/SingleAdvertBidForm.js
@@ -1,64 +1,27 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { placeBid } from '../../actions/advert';
 import { Fragment } from 'react';
-import { Modal } from 'react-bootstrap';
+import { setAlert } from '../../actions/alert';
 
-export const SingleAdvertBidForm = ({ advertId, placeBid }) => {
+export const SingleAdvertBidForm = ({ advertId, placeBid, setAlert }) => {
   const [bid, setBid] = useState('');
-  const [smShow, setSmShow] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  // console.log(bid);
 
-  console.log(bid);
-
-  const myModal = (
-    <Modal
-      size='sm'
-      show={smShow}
-      onHide={() => setSmShow(false)}
-      aria-labelledby='example-modal-sizes-title-sm'
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id='example-modal-sizes-title-sm'>Success!</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>You have successfully placed your bid of {bid}. </Modal.Body>
-    </Modal>
-  );
   const submitBid = (e) => {
     e.preventDefault();
-    if (typeof bid === 'string') {
+    let isnum = /^\d+$/.test(bid);
+    if (isnum) {
       placeBid(advertId, { bid: bid });
+      setAlert(`You have successfully placed your bid of ${bid}`, 'success');
       setBid('');
-      // setTimeout(() => alert('Success!'), 2000);
-      // setTimeout(() => myModal, 2000);
-      setShowModal(true);
-    } else {
-      myModal('fail');
+    } else if (!isnum) {
+      setAlert('Please try again.', 'danger');
     }
   };
   return (
     <Fragment>
-      {showModal && (
-        <Fragment>
-          <Modal
-            size='sm'
-            show={smShow}
-            onHide={() => setSmShow(false)}
-            aria-labelledby='example-modal-sizes-title-sm'
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id='example-modal-sizes-title-sm'>
-                Success!
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              You have successfully placed your bid of {bid}.{' '}
-            </Modal.Body>
-          </Modal>
-        </Fragment>
-      )}
-
       <form onSubmit={(e) => submitBid(e)}>
         <div className='single-advert-bid'>
           <input
@@ -80,6 +43,7 @@ export const SingleAdvertBidForm = ({ advertId, placeBid }) => {
 
 SingleAdvertBidForm.propTypes = {
   placeBid: PropTypes.func,
+  setAlert: PropTypes.func,
 };
 
-export default connect(null, { placeBid })(SingleAdvertBidForm);
+export default connect(null, { placeBid, setAlert })(SingleAdvertBidForm);
