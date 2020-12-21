c2a68c0f4e272dbc608f2fe4318706bb98dc19f4
sbayrak <suat.bayrak@bilgiedu.net>
2020-12-21 18:34:39 +0300

    Working on redirecting after submit of new advert

--- client/src/components/adverts/CreateAdvert.js
+++ client/src/components/adverts/CreateAdvert.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../css/cradvert.css';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addAdvert } from '../../actions/advert';
+import { getAdverts } from '../../actions/advert';
 import { setAlert } from '../../actions/alert';
+import { Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import { Redirect } from 'react-router-dom';
 
-const CreateAdvert = ({ addAdvert, setAlert }) => {
+const CreateAdvert = ({
+  addAdvert,
+  setAlert,
+  getAdverts,
+  advert: { adverts, loading },
+}) => {
   const [title, setTitle] = useState('');
   const [text, setText] = useState('');
   const [location, setLocation] = useState('');
+  const [submitRedirect, setSubmitRedirect] = useState(false);
+
+  useEffect(() => {
+    getAdverts();
+  }, []);
 
   const submitNewAdvert = (e) => {
     e.preventDefault();
@@ -38,62 +52,94 @@ const CreateAdvert = ({ addAdvert, setAlert }) => {
       setText('');
       setTitle('');
       setAlert('You have successfully posted your advert!', 'success');
+      setSubmitRedirect(true);
     }
   };
+  let lastElement = 0;
+  if (adverts !== null) {
+    lastElement = adverts[adverts.length - 1];
+  }
 
   return (
-    <div className='section'>
-      <div className='create-advert'>
-        <span className='section-title'>Create Advert</span>
-        <form
-          className='create-advert-form'
-          onSubmit={(e) => submitNewAdvert(e)}
-        >
-          <div className='create-advert-top'>
-            <label htmlFor='title' className='create-advert-lbl'>
-              Title{' '}
-            </label>
-            <input
-              type='text'
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Please enter advert's title"
-            />
+    <Fragment>
+      {loading ? (
+        <Spinner></Spinner>
+      ) : (
+        <Fragment>
+          <div className='section'>
+            <div className='create-advert'>
+              <span className='section-title'>Create Advert</span>
+              {adverts !== null ? (
+                <Fragment>
+                  <form
+                    className='create-advert-form'
+                    onSubmit={(e) => submitNewAdvert(e)}
+                  >
+                    <div className='create-advert-top'>
+                      <label htmlFor='title' className='create-advert-lbl'>
+                        Title{' '}
+                      </label>
+                      <input
+                        type='text'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Please enter advert's title"
+                      />
+                    </div>
+                    <div className='create-advert-mid'>
+                      <label htmlFor='text' className='create-advert-lbl'>
+                        Description{' '}
+                      </label>
+                      <textarea
+                        name='text'
+                        cols='30'
+                        rows='10'
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder='Please enter a clear description'
+                      ></textarea>
+                    </div>
+                    <div className='create-advert-bottom'>
+                      <label htmlFor='location' className='create-advert-lbl'>
+                        Location{' '}
+                      </label>
+                      <input
+                        type='text'
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        placeholder='Please enter location'
+                      />
+                    </div>
+                    <button type='submit'>Submit</button>
+                  </form>
+                  {submitRedirect && (
+                    <Redirect
+                      to={`/adverts/advert/${lastElement._id}`}
+                    ></Redirect>
+                  )}
+                </Fragment>
+              ) : (
+                <Spinner></Spinner>
+              )}
+            </div>
           </div>
-          <div className='create-advert-mid'>
-            <label htmlFor='text' className='create-advert-lbl'>
-              Description{' '}
-            </label>
-            <textarea
-              name='text'
-              cols='30'
-              rows='10'
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              placeholder='Please enter a clear description'
-            ></textarea>
-          </div>
-          <div className='create-advert-bottom'>
-            <label htmlFor='location' className='create-advert-lbl'>
-              Location{' '}
-            </label>
-            <input
-              type='text'
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-              placeholder='Please enter location'
-            />
-          </div>
-          <button type='submit'>Submit</button>
-        </form>
-      </div>
-    </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
 CreateAdvert.propTypes = {
   addAdvert: PropTypes.func,
   setAlert: PropTypes.func,
+  advert: PropTypes.object,
+  getAdverts: PropTypes.func,
 };
 
-export default connect(null, { addAdvert, setAlert })(CreateAdvert);
+const mapStateToProps = (state) => ({
+  advert: state.advert,
+});
+
+export default connect(mapStateToProps, { addAdvert, setAlert, getAdverts })(
+  CreateAdvert
+);
