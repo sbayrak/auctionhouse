d53d18bdce09a2c1b6f64b2de14e39cd8983251d
sbayrak <34121748+sbayrak@users.noreply.github.com>
2020-12-25 11:49:50 +0300

    prepare for deploy v3

---
+++ client/build/precache-manifest.9445c96f9cd63b29c0205079e3898758.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4926fc65340a8daf3493160f957c3d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b280ca0127605d4137d9",
+    "url": "/static/css/main.d3dbf059.chunk.css"
+  },
+  {
+    "revision": "7bbb1409f1dcc1ccd910",
+    "url": "/static/js/2.b4e6ebc9.chunk.js"
+  },
+  {
+    "revision": "07d28acefe4f478bb7a9400a0790acb9",
+    "url": "/static/js/2.b4e6ebc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b280ca0127605d4137d9",
+    "url": "/static/js/main.7fc47d83.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "70219a79d541675c76898b2c25ccd7b1",
+    "url": "/static/media/agreement.70219a79.svg"
+  },
+  {
+    "revision": "adb844ced015034111b7433a9ac77327",
+    "url": "/static/media/bid.adb844ce.svg"
+  },
+  {
+    "revision": "2aa9d48fb0f63733d78b4e637f5102a6",
+    "url": "/static/media/calendar.2aa9d48f.svg"
+  },
+  {
+    "revision": "fa2ed7b85005ecaf49b4ba8c8cb3bdeb",
+    "url": "/static/media/conference.fa2ed7b8.svg"
+  },
+  {
+    "revision": "1f79af8e76f97e20afd426f0d9f3edb6",
+    "url": "/static/media/deal3.1f79af8e.svg"
+  },
+  {
+    "revision": "741b73199ff4e165e11ce45ada6bb31d",
+    "url": "/static/media/secure.741b7319.svg"
+  },
+  {
+    "revision": "8820e641451be0344be38cfb4f388cf1",
+    "url": "/static/media/security.8820e641.svg"
+  },
+  {
+    "revision": "a32792fdc9ae8c588e18fbf75c33cef7",
+    "url": "/static/media/signin.a32792fd.svg"
+  },
+  {
+    "revision": "7cfd4e21416983efa66da91cb44b9c29",
+    "url": "/static/media/signup.7cfd4e21.svg"
+  },
+  {
+    "revision": "9311dab586c6f10cbc63f2c86e7db141",
+    "url": "/static/media/signup_mobile.9311dab5.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -18,5 +18,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/adverts', require('./routes/api/adverts'));
 //app.use('/api/adverts/', require('./routes/api/adverts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
